9d15cce94d28bec0693ac82fcfce0a4e8a1dbb66
DavidPalaciosAtos <david.palacios.external@atos.net>
2023-03-21 09:13:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/actividad5_retrofit/pokeapi/PokeapiService.java
@@ -0,0 +1,14 @@
+package com.example.actividad5_retrofit.pokeapi;
+
+import com.example.actividad5_retrofit.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeapiService {
+
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit") int limit, @Query("offset") int offset);
+
+}
